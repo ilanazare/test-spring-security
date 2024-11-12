96ca788bf222a55179e869f4679db8768fd672b2
ilanazare <ivanazare94@gmail.com>
2024-11-12 09:52:16 -0300

    adds others controller

--- src/main/kotlin/com/security/spring/controller/ContactController.kt
+++ src/main/kotlin/com/security/spring/controller/ContactController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/welcome")
-class WelcomeController {
+class ContactController  {
     @GetMapping
-    fun welcome(): String = "Welcome to Spring Application with security"
+    fun contact(): String = "Welcome to Spring Application with security"
 }
